c21d20e79a98b5424495ef03eb6fee18054664d4
Mao <doryantilmant25@gmail.com>
2024-10-05 02:48:54 +0200

    fix: overlapping props

--- assets/temporary/spawnable_zone.gd
+++ assets/temporary/spawnable_zone.gd
@@ -2,39 +2,51 @@ extends NavigationRegion3D
 
 @export var zone_props: Array[PackedScene]
 @export var test_prop: PackedScene
+@export var count: int = 8
 
-var map: RID
+func _ready() -> void:		
+	await get_tree().physics_frame 
+	await get_tree().physics_frame
+	
+	var rng = RandomNumberGenerator.new()
+	
+	for i in range(rng.randi_range(0, count)):
+		spawn_at_random_pos(zone_props.pick_random())
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	#for i in range(navigation_mesh.get_polygon_count()):
-		#print(navigation_mesh.get_polygon(i))
-		#var pol = navigation_mesh.get_polygon(i)
-		#var prop = test_prop.instantiate()
-		#add_child(prop)
-		#prop.position = Vector3(pol[0], pol[1], pol[2])
-		
-	map = NavigationServer3D.map_create()
-	NavigationServer3D.region_set_map(get_rid(), map)
-	
-	for i in range(len(zone_props)):
-		spawn_at_random_pos(zone_props[i])
-	
-
-func spawn_at_random_pos(prop: PackedScene):
-	NavigationServer3D.map_force_update(map)
-	var pos = NavigationServer3D.map_get_random_point(map, 1, false)
-	
-	var prop_instance = prop.instantiate() as StaticBody3D
-	add_child(prop_instance)
-	#pos.y = 2
-	#print(pos)
-	
-	prop_instance.global_position = pos
-
-func get_random_zone_prop():
-	return zone_props.pick_random()
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("right"):
 		get_tree().reload_current_scene()
+
+
+func spawn_at_random_pos(prop: PackedScene):	
+	var prop_instance = prop.instantiate() as StaticBody3D
+	add_child(prop_instance)
+	
+	prop_instance.global_position = get_random_pos()
+	while await is_prop_colliding(prop_instance):
+		prop_instance.global_position = get_random_pos()
+
+
+func get_random_zone_prop():
+	return zone_props.pick_random()
+
+
+func get_random_pos() -> Vector3:
+	return NavigationServer3D.region_get_random_point(get_rid(), 1, false)
+
+
+func is_prop_colliding(prop_instance: StaticBody3D) -> bool:
+	var prop_collision: CollisionShape3D = prop_instance.get_node("Collision")
+	
+	var overlap_query := PhysicsShapeQueryParameters3D.new()
+	overlap_query.collide_with_bodies = true
+	overlap_query.transform = prop_instance.global_transform
+	overlap_query.shape = prop_collision.shape
+	overlap_query.collision_mask = 0b10100
+	overlap_query.exclude = [prop_instance.get_rid()]
+	overlap_query.margin = .2
+	
+	await get_tree().physics_frame 
+	var overlaps = get_world_3d().direct_space_state.intersect_shape(overlap_query)
+	return overlaps.size() > 0
